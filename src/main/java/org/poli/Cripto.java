799b9dbd4a2ca50f91211a8f37c0a3bcec249d00
Joaquin <alumno24.bardelli.joaquin@ipm.edu.ar>
2023-09-02 18:31:13 -0300

    Subo codigo encriptacion

--- src/main/java/org/poli/Cripto.java
+++ src/main/java/org/poli/Cripto.java
@@ -1,6 +1,12 @@
 package org.poli;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.nio.charset.StandardCharsets;
 import java.security.*;
+import java.util.Arrays;
 
 public class Cripto {
     /*private final KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
@@ -18,4 +24,24 @@ public class Cripto {
     }
 
     // Falta hacer un metodo que encripte y otro que desencripte
+    public byte[] encriptar(String mensaje) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher encryptCipher = Cipher.getInstance("RSA");
+        encryptCipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte [] mensajeBytes = mensaje.getBytes(StandardCharsets.UTF_8);
+        byte[] mensajeEncriptadoBytes =encryptCipher.doFinal(mensajeBytes);
+        System.out.println(Arrays.toString(mensajeEncriptadoBytes));
+        return mensajeEncriptadoBytes;
+    }
+    public void desencriptar(byte[] mensajeEncriptadoBytes) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher decryptCipher = Cipher.getInstance("RSA");
+        decryptCipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] mensajeDesencriptadoBytes = decryptCipher.doFinal(mensajeEncriptadoBytes);
+        String mensajeDesencriptado = new String(mensajeDesencriptadoBytes, StandardCharsets.UTF_8);
+        System.out.println(mensajeDesencriptado);
+    }
+    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+        Cripto cripto = new Cripto();
+        byte[] mensajeEncriptado = cripto.encriptar("Hola perro");
+        cripto.desencriptar(mensajeEncriptado);
+    }
 }
